8492a96f6b8c32f2af6aee272350cea32bce1741
alvina893 <alvinaleony293@gmail.com>
2025-06-15 17:53:35 +0700

    whymustutestme

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div>hi there, i am tired of college, can't wait until i graduate</div>
+  );
 };
 
 export default Test;
